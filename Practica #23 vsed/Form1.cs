388e46e626fa2cb2f02fc421f64be76a4c86627e
Cooper858 <bryanslymc@gmail.com>
2024-11-14 12:50:33 -0600

    Add files via upload

---
+++ Practica #23 vsed/Form1.cs
@@ -0,0 +1,30 @@
+using System.Xml.Schema;
+
+namespace Practica__23_vsed
+{
+    public partial class Form1 : Form
+    {
+        float f_num, Total = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Boton Sumarnumeros
+            textBox2.Text = Total.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Boton Añadir
+            listBox1.Items.Add(textBox1.Text);
+            //Aqui es donde pasamos el parametro
+            f_num = float.Parse(textBox1.Text);
+            Total += f_num;
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
